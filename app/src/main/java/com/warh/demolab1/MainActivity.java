ad174754c174d9e2d692d678a4ae98d404faa713
WilliHominal <willi.hominal97@gmail.com>
2021-08-28 20:50:18 -0300

    Spinner agregado

--- app/src/main/java/com/warh/demolab1/MainActivity.java
+++ app/src/main/java/com/warh/demolab1/MainActivity.java
@@ -2,12 +2,27 @@ package com.warh.demolab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner categoriaSpinner;
+    ArrayAdapter<CharSequence> adapterCategoriaSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        categoriaSpinner = (Spinner) findViewById(R.id.categoriaSpinner);
+        adapterCategoriaSpinner = ArrayAdapter.createFromResource(this, R.array.categorias, android.R.layout.simple_spinner_dropdown_item);
+
+        categoriaSpinner.setAdapter(adapterCategoriaSpinner);
+        categoriaSpinner.setPrompt(getResources().getString(R.string.categoria_label).toUpperCase());
+
     }
 }
